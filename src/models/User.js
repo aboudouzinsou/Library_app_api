df26f5f599f3902c1370c51b403fa62518ac42e0
aboudouzinsou <faboudouzinsou@gmail.com>
2024-09-07 00:40:32 +0200

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -19,6 +19,10 @@ const UserSchema = new mongoose.Schema({
     enum: ['user', 'admin'],
     default: 'user',
   },
+  points: {
+    type: Number,
+    default: 100, // Initial points
+  },
 });
 
 module.exports = mongoose.model('User', UserSchema);
