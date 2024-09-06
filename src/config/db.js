08b55915b4a128df854d2e4bd672e504379c5887
aboudouzinsou <faboudouzinsou@gmail.com>
2024-09-06 23:50:33 +0200

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,3 @@
-// src/config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
---
+++ src/utils/sendEmail.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (to, subject, text) => {
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  let mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: to,
+    subject: subject,
+    text: text,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Email sent: ' + info.response);
+  });
+};
+
+module.exports = sendEmail;
