9eed8faf72f25de153a4fbadb19aa068b2082db5
aboudouzinsou <faboudouzinsou@gmail.com>
2024-09-07 00:27:43 +0200

    Update swagger.js

--- src/swagger.js
+++ src/swagger.js
@@ -1,4 +1,3 @@
-// src/swagger.js
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
@@ -8,6 +7,64 @@ const options = {
     info: {
       title: 'Library Management API',
       version: '1.0.0',
+    },
+    components: {
+      schemas: {
+        Book: {
+          type: 'object',
+          properties: {
+            title: {
+              type: 'string',
+            },
+            author: {
+              type: 'string',
+            },
+            genre: {
+              type: 'string',
+            },
+            available: {
+              type: 'boolean',
+            },
+          },
+        },
+        User: {
+          type: 'object',
+          properties: {
+            name: {
+              type: 'string',
+            },
+            email: {
+              type: 'string',
+            },
+            password: {
+              type: 'string',
+            },
+            role: {
+              type: 'string',
+              enum: ['user', 'admin'],
+            },
+          },
+        },
+        Loan: {
+          type: 'object',
+          properties: {
+            book: {
+              type: 'string',
+            },
+            user: {
+              type: 'string',
+            },
+            borrowDate: {
+              type: 'string',
+              format: 'date-time',
+            },
+            returnDate: {
+              type: 'string',
+              format: 'date-time',
+            },
+          },
+        },
+      },
     },
   },
   apis: ['./routes/*.js'],
