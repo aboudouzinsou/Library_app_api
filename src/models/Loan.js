57944650b5608ce9f41c01b7d20655f052357eb7
aboudouzinsou <faboudouzinsou@gmail.com>
2024-09-07 00:22:53 +0200

    Create Loan.js
    creating loan model

---
+++ src/models/Loan.js
@@ -0,0 +1,25 @@
+// src/models/Loan.js
+const mongoose = require('mongoose');
+
+const LoanSchema = new mongoose.Schema({
+  book: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Book',
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  borrowDate: {
+    type: Date,
+    required: true,
+  },
+  returnDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Loan', LoanSchema);
